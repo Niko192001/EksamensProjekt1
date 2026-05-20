041f9eb31a6c3dfde1e8a832957968fa4342e810
Ali hassan <alha0014@stud.ek.dk>
2026-05-20 10:47:49 +0200

    1 fitscreen

--- app/src/main/java/com/example/neveranother/screens/FitScreen.kt
+++ app/src/main/java/com/example/neveranother/screens/FitScreen.kt
@@ -1,5 +1,10 @@
 package com.example.neveranother.screens
+
 import android.accessibilityservice.GestureDescription
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+import com.example.neveranother.R
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -11,13 +16,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
 
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -39,13 +45,16 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.ui.draw.shadow
+
 @OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
-fun Fitscreen(){
+fun FitScreen() {
     Scaffold(
         topBar = {
-            TopAppBar(
-                title = { Text("FIT")},
+            CenterAlignedTopAppBar(
+                title = { Text("FIT") },
 
                 navigationIcon = {
                     IconButton(onClick = {}) {
@@ -56,7 +65,7 @@ fun Fitscreen(){
                 }
             )
         }
-    ) { innerpadding  ->
+    ) { innerpadding ->
         Column(
             modifier = Modifier
                 .padding(innerpadding)
@@ -72,35 +81,62 @@ fun Fitscreen(){
             )
             Spacer(modifier = Modifier.height(24.dp))
             Text("Hvordan vil du oplyse dine mål?", fontWeight = FontWeight.Bold)
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(150.dp))
 
             OptionCard(
+
                 title = "Skriv dine mål ind",
                 description = "Indtast dine mål manuelt, så vi kan finde den bedste pasform til dig.",
-                onClick = {}
-            )
-            Spacer(modifier = Modifier.height(16.dp))
+                onClick = {},
+                imageRes = R.drawable.kropmaal,
+
+                )
+            Spacer(modifier = Modifier.height(50.dp))
             OptionCard(
                 title = "Scan din krop",
                 description = "Brug vores scanning til at få dine mål præcist og hurtigt.",
-                onClick = {}
-            )
-        } }
+                onClick = {},
+                imageRes = R.drawable.krop_ikon,
+
+                )
+        }
     }
+}
 
 @Composable
-fun OptionCard(title: String, description: String, onClick: () -> Unit) {
+fun OptionCard(title: String, description: String, imageRes: Int, onClick: () -> Unit) {
     Card(
-        modifier = Modifier.fillMaxWidth().clickable { onClick() },
+        modifier = Modifier
+            .fillMaxWidth()
+            .shadow(80.dp)
+            .clickable { onClick() },
         shape = RoundedCornerShape(12.dp),
+
         colors = CardDefaults.cardColors(containerColor = Color(0xFFF5F0EC))
+
     ) {
-        Row(modifier = Modifier.padding(16.dp), verticalAlignment = Alignment.CenterVertically) {
-            Box(modifier = Modifier.size(56.dp).background(Color(0xFFE8DDD5), CircleShape))
-            Spacer(modifier = Modifier.width(16.dp))
+        Row(modifier = Modifier.padding(40.dp), verticalAlignment = Alignment.CenterVertically) {
+            Box(
+                modifier = Modifier
+                    .size(70.dp)
+                    .background(Color(0xFFE8DDD5), CircleShape),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = imageRes),
+                    contentDescription = null,
+                    modifier = Modifier.size(45.dp)
+                )
+            }
+            Spacer(
+                modifier = Modifier
+                    .width(16.dp)
+                    .shadow(500.dp)
+            )
             Column {
                 Text(title, fontWeight = FontWeight.Bold, fontSize = 16.sp)
                 Text(description, fontSize = 13.sp)
+
             }
         }
     }
