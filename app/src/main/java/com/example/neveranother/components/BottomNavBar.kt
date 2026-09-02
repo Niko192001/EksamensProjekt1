85b78d0f6d8c9b5e8abf8210072fc96c9267c528
nikolajrahe <nikolaj@rahe.dk>
2026-09-02 18:19:42 +0200

    Newest changes to the project

--- app/src/main/java/com/example/neveranother/components/BottomNavBar.kt
+++ app/src/main/java/com/example/neveranother/components/BottomNavBar.kt
@@ -2,7 +2,7 @@ package com.example.neveranother.components
 
 import androidx.compose.ui.graphics.vector.ImageVector
 
-data class BottomNavBar(
+class BottomNavBar(
     val route: String,
     val icon: ImageVector,
     val label: String
--- app/src/main/java/com/example/neveranother/models/FitViewModel.kt
+++ app/src/main/java/com/example/neveranother/models/FitViewModel.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class FitViewModel : ViewModel() {
+    // Det er en string type, men
     var upperValue by mutableStateOf("")
     var lowerValue by mutableStateOf("")
     var spanValue by mutableStateOf("")
